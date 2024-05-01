ef7d05b028a7dd1aa664e1a414300286efc1f43a
Бабко Евгений <doxis32123@gmail.com>
2024-05-01 16:14:52 +0300

    task1_ready

---
+++ WebDriwerTask1/Tests/Tests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriwer.Task1;
+
+namespace Tests
+{
+    public class Tests
+    {
+        private IWebDriver driver;
+        private PastebinPage pastebinPage;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            pastebinPage = new PastebinPage(driver);
+        }
+
+        [Test]
+        public void Test_CreateNewPaste()
+        {
+            pastebinPage.PostCode("helloweb", "Hello from WebDriver");
+
+            ClassicAssert.AreEqual("Hello from WebDriver", pastebinPage.Code());
+            ClassicAssert.AreEqual("helloweb", pastebinPage.Title());
+            ClassicAssert.AreEqual("10 Minutes", pastebinPage.Expiration());
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ WebDriwerTask1/WebDriwer.Task1/PastebinPage.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium;
+
+namespace WebDriwer.Task1
+{
+    public class PastebinPage
+    {
+        private IWebDriver driver;
+        private IWebElement text;
+        private IWebElement title;
+        private IWebElement expirationContainer;
+        private IWebElement expirationTenMin;
+
+        public PastebinPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            driver.Navigate().GoToUrl("https://pastebin.com/");
+            text = driver.FindElement(By.Id("postform-text"));
+            title = driver.FindElement(By.Id("postform-name"));
+            expirationContainer = driver.FindElement(By.Id("select2-postform-expiration-container"));
+        }
+
+        public void PostCode(string name, string code)
+        {
+            text.SendKeys(code);
+            title.SendKeys(name);
+            expirationContainer.Click();
+
+            expirationTenMin = driver.FindElement(By.XPath("/html/body/span[2]/span/span[2]/ul/li[3]"));
+            expirationTenMin.Click();
+        }
+
+        public string Code()
+        {
+            return text.GetAttribute("value");
+        }
+
+        public string Title()
+        {
+            return title.GetAttribute("value");
+        }
+
+        public string Expiration()
+        {
+            return expirationContainer.Text;
+        }
+    }
+}
